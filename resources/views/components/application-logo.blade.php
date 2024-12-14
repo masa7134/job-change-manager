5e91226cc5964ca9888a9b8884019a08fb0a37ac
masa7134 <masa7134@gmail.com>
2024-12-14 17:58:30 +0900

    ロゴ調整

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,15 @@
-<img src="{{ asset("images/logo.png" )}}">
+@php
+    // ログイン画面かどうかを判定
+    $isLoginPage = request()->route()->getName() === 'login'
+        || request()->is('login');
+
+    // ログイン画面の場合は大きいサイズ、それ以外は小さいサイズを設定
+    $dimensions = $isLoginPage ? '160' : '65';
+    $classes = $isLoginPage ? 'mx-auto' : '';
+@endphp
+
+<img src="{{ asset("images/logo.png") }}"
+    width="{{ $dimensions }}"
+    height="{{ $dimensions }}"
+    class="object-contain {{ $classes }}"
+>
