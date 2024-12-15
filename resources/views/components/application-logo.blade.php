fa03e72a522738a79391763bdebb03769ff97a7c
masa7134 <masa7134@gmail.com>
2024-12-15 14:09:17 +0900

    welcome画面作成

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,11 +1,16 @@
 @php
-    // ログイン画面かどうかを判定
-    $isLoginPage = request()->route()->getName() === 'login'
-        || request()->is('login');
+    // ページの判定
+    $currentRouteName = request()->route()->getName();
+    $isLoginOrWelcome = $currentRouteName === 'login'
+        || request()->is('login')
+        || $currentRouteName === 'register'
+        || request()->is('register')
+        || $currentRouteName === 'welcome'
+        || request()->is('/');
 
     // ログイン画面の場合は大きいサイズ、それ以外は小さいサイズを設定
-    $dimensions = $isLoginPage ? '160' : '65';
-    $classes = $isLoginPage ? 'mx-auto' : '';
+    $dimensions = $isLoginOrWelcome ? '160' : '65';
+    $classes = $isLoginOrWelcome ? 'mx-auto' : '';
 @endphp
 
 <img src="{{ asset("images/logo.png") }}"
