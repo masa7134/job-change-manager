8fb6d58db2d2597b2594816b05cf4a10f397b808
Masa 7134 <masa@mba.local>
2025-01-16 16:32:45 +0900

    ナビリンク位置変更、ページネーション修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
+            <div class="flex items-center justify-between flex-1">
                 <!-- Logo -->
                 <div class="flex items-center">
                     <a href="{{ route('dashboard') }}">
@@ -11,11 +11,18 @@
                 </div>
 
                 <!-- Navigation Links -->
-                {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('進捗状況') }}
-                    </x-nav-link>
-                </div> --}}
+                <div class="flex items-center space-x-4">
+                    <a href="{{ route('company.register') }}"
+                        class="bg-gray-500 text-white text-sm px-4 py-2 rounded hover:bg-gray-700">
+                        企業を登録する
+                    </a>
+                    <a href="{{ route('company.all') }}"
+                        class="bg-gray-500 text-white text-sm px-4 py-2 rounded hover:bg-gray-700">
+                        全企業を表示する
+                    </a>
+                </div>
+
+
             </div>
 
             <!-- Settings Dropdown -->
